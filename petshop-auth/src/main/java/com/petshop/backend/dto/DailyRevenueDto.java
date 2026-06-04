fe40e4998447213e84cd55fcdf22fbc4e67ac524
kiet123839 <thanhnhayvn@gmail.com>
2026-06-04 16:04:30 +0700

    feat: restore main branch

---
+++ petshop-auth/src/main/java/com/petshop/backend/dto/DailyRevenueDto.java
@@ -0,0 +1,34 @@
+package com.petshop.backend.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class DailyRevenueDto {
+    private LocalDate ngayDoanhThu;
+    private Integer tongDonHang;
+    private BigDecimal tongDoanhThu;
+    private Integer khachHangDuyNhat;
+
+    public DailyRevenueDto(LocalDate ngayDoanhThu, Integer tongDonHang, BigDecimal tongDoanhThu, Integer khachHangDuyNhat) {
+        this.ngayDoanhThu = ngayDoanhThu;
+        this.tongDonHang = tongDonHang;
+        this.tongDoanhThu = tongDoanhThu;
+        this.khachHangDuyNhat = khachHangDuyNhat;
+    }
+
+    public LocalDate getNgayDoanhThu() {
+        return ngayDoanhThu;
+    }
+
+    public Integer getTongDonHang() {
+        return tongDonHang;
+    }
+
+    public BigDecimal getTongDoanhThu() {
+        return tongDoanhThu;
+    }
+
+    public Integer getKhachHangDuyNhat() {
+        return khachHangDuyNhat;
+    }
+}
